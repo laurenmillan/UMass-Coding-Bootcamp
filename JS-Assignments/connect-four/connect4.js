13c103f65b93c9171036df5335e7ef9a18170652
Lauren M <109766803+mlauren77@users.noreply.github.com>
2022-10-06 16:52:16 -0400

    Web (#3)
    * fixed tied function, win function, fixed css
    
    * added comments to code
    
    * created reset game function, modified comments, modified css
    
    * added notes for how the web works

--- JS-Assignments/connect-four/connect4.js
+++ JS-Assignments/connect-four/connect4.js
@@ -12,8 +12,6 @@ const HEIGHT = 6; // y, rows
 
 let board = []; // array of rows, each row is an array of cells  (board[y][x])
 
-const reset = document.getElementById("#reset"); // reset button
-
 // this function creates a 2d matrix by creating new arrays using the constructor: Array, and allocates memory for HEIGHT and WIDTH
 
 function makeBoard() {
@@ -21,15 +19,16 @@ function makeBoard() {
   // here we grab the elements of height and width and put it in constructor: Array
   board = new Array(HEIGHT); // create memory to contain size 6
   for (let h = 0; h < HEIGHT; h++) { // loop through the rows
-    board[h] = new Array(WIDTH); // create memory to contain size 7
+    board[h] = new Array(WIDTH); // allocates memory to contain size 7
   }
-  // nested loops so we can access every single element
+  // nested loops to access every single element
   for (let h = 0; h < HEIGHT; h++) { // this loop populates HEIGHT/rows
     for (let w = 0; w < WIDTH; w++) { // this loop populates WIDTH/columns
       board[h][w] = null; // take board and set it to null, it is set to null because it does not currently have any value
     }
   } 
 }
+
 
 // The makeHtmlBoard function dynamically creates the HTML table of rows and columns, with HEIGHT num of rows, WIDTH num of columns. For each iteration in HEIGHT, a new row is generated. For that row, we loop through width to create width num of cells(columns) and append it to the row(td). When the WIDTH loop finishes, the code appends the row with the newly created table data cells to the board.
 
@@ -40,50 +39,56 @@ function makeHtmlBoard() {
 
   const top = document.createElement("tr");
   top.setAttribute("id", "column-top"); 
+
   // when eventListener hears click, we call handleClick on top row of board
   top.addEventListener("click", handleClick); 
 
-  for (let x = 0; x < WIDTH; x++) { // loop through indices of x-axis/columns
+  for (let x = 0; x < WIDTH; x++) { // loop through indices of columns
     const headCell = document.createElement("td"); 
     headCell.setAttribute("id", x); 
     top.append(headCell);  
   }
   htmlBoard.append(top); // creates and appends top row to board 
   
-  for (let y = 0; y < HEIGHT; y++) {  // loops through y-axis of rows
+  for (let y = 0; y < HEIGHT; y++) {  // loops through rows
     const row = document.createElement("tr"); 
-    for (let x = 0; x < WIDTH; x++) { 
+    for (let x = 0; x < WIDTH; x++) { // nested loops to access every single element
       const cell = document.createElement("td"); 
-      cell.setAttribute("id", `${y}-${x}`);  
+      cell.setAttribute("id", `${y}-${x}`); // sets the value of `${y}-${x}` on every element
       row.append(cell);
     }
-    htmlBoard.append(row); // appends rows to board
+    htmlBoard.append(row);
   }
 }
 
+
 /** handleClick: handle click of column top to play coin */
 
-// define handleClick function
 function handleClick(evt) {
-  // this extracts the id of clicked target and outputs a string. The unary operation converts the string to integers
+  // this extracts the id of clicked target and outputs a string. The unary operation converts the string to an integer
   let x = +evt.target.id;
 
-  // get next spot in column (if none, ignore click)
+  // get next spot in column
   let y = findSpotForCol(x);
+  // console.log(x);
   if (y === null) {
     return;
   }
-
   // place coin in board and add to HTML table
   placeInTable(y, x);
 
   // check for win
   if (checkForWin()) {
-    return endGame(`Player ${currPlayer} won!`);
+    endGame(`Player ${currPlayer} won!`);
+    return;
   } 
-  
+  else if (tiedGame()) {
+    alert("Game is a Tie!");
+    return;
+  }
   // alternate players using ternary operation
-  currPlayer = currPlayer === 1 ? 2 : 1; // basically an if else statement
+  // if condition is true, player1 plays, if condition is false, player2 plays (if else statement)
+  currPlayer = currPlayer === 1 ? 2 : 1; 
 }
 
 /** findSpotForCol: given column x, return top empty y (null if filled) */
@@ -92,6 +97,7 @@ function findSpotForCol(x) {
   // coins start at bottom row of board 
   for (let r = HEIGHT -1; r >= 0; r--) { 
     if (board[r][x] === null) {
+      // console.log(r,x);
       return r;
     }
   } // end of loop
@@ -99,7 +105,7 @@ function findSpotForCol(x) {
   return null;
 }
 
-/** placeInTable: update DOM to place coin into HTML table of board */
+/** placeInTable: update DOM to place coin into HTML board */
 function placeInTable(y, x) {
   // set element at y, x location to current player(blue or red)
   board[y][x] = currPlayer;
@@ -111,7 +117,7 @@ function placeInTable(y, x) {
   // search for the element ${y}-${x}
   const space = document.getElementById(`${y}-${x}`);
 
-  // append the coin to the HTML table
+  // append the coin to the HTML board
   space.append(coin);
 }
 
@@ -120,15 +126,15 @@ function placeInTable(y, x) {
 function tiedGame() {
   for (let y = 0; y < HEIGHT; y++) {
     for (let x = 0; x < WIDTH; x++) {
-      if (board[y][x] === null) {
-        return; // if a single null is found, the function exits 
-      }
+      if (board[y][x] === null) // null implies spot on board is still empty and can be filled {
+        return false; // if a single null is found, the function exits 
     }
   }
-  alert("Game is a Tie!");
+  return true;
 }
 
-// this function announces game end
+
+// this function announces game end;; it's a void function
 function endGame(msg) {
   alert(msg);
 } 
@@ -141,7 +147,10 @@ function checkForWin() {
     //  - cells: list of four (y, x) cells
     //  - returns true if all are legal coordinates & all match currPlayer
 
-    // every() method tests whether all elements in the array pass the tests implemented by the function win(cells)
+  
+    // the every() method function is a member function to array, the input to the function are the elements. It returns a Boolean value.
+    // it tests the array of [y,x] and checks whether all the elements in the array pass the tests below, which returns true, if it fails, it returns false. 
+    // The input is the matrix which is array of arrays
     return cells.every( 
       ([y, x]) =>
         y >= 0 &&
@@ -169,14 +178,23 @@ function checkForWin() {
       }
     }
   }
+  return false;
 }
 
-function resetGame() {
-  document.querySelector("#reset").addEventListener("click");
+const reset = document.getElementById("reset"); // reset button to restart Game
+// restart Game
+reset.addEventListener("click", () => {
+    // reset game variables
+    board = [];
+    currPlayer = 1;
+    // reset game board
+    headCell.forEach(cells => {
+      cells.classList.remove("id");
+    });
+});
   
-}
 
-// this function generates random colors for h1 
+// this function generates random colors for the h1 
 function randomRGB() {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
@@ -190,7 +208,7 @@ setInterval(function(){
   for (let letter of letters) {
     letter.style.color = randomRGB();
   }
-},500)
+},500) //timeout interval
 
 makeBoard();
 makeHtmlBoard();
