107ecc0b1475ebe9280bc61c2e390ac2faa86509
Lauren Millan <laurenmillan@mydsn.net>
2022-10-23 11:04:29 -0400

    started project, added getCategory function

---
+++ JS-Assignments/jeopardy-assessment/jeopardy.js
@@ -0,0 +1,121 @@
+// categories is the main data structure for the app; it looks like this:
+
+//  [
+//    { title: "Math",
+//      clues: [
+//        {question: "2+2", answer: 4, showing: null},
+//        {question: "1+1", answer: 2, showing: null}
+//        ...
+//      ],
+//    },
+//    { title: "Literature",
+//      clues: [
+//        {question: "Hamlet Author", answer: "Shakespeare", showing: null},
+//        {question: "Bell Jar Author", answer: "Plath", showing: null},
+//        ...
+//      ],
+//    },
+//    ...
+//  ]
+const game = document.getElementById('game');
+let categories = [];
+const NUM_CATEGORIES = 6;
+const NUM_QUESTIONS_PER_CAT = 5;
+
+/** Get NUM_CATEGORIES random category from API.
+ *
+ * Returns array of category ids
+ */
+
+async function getCategoryIds() {
+	let res = await axios.get('https://jservice.io/api/categories?count=100');
+	console.log(res);
+}
+
+/** Return object with data about a category:
+ *
+ *  Returns { title: "Math", clues: clue-array }
+ *
+ * Where clue-array is:
+ *   [
+ *      {question: "Hamlet Author", answer: "Shakespeare", showing: null},
+ *      {question: "Bell Jar Author", answer: "Plath", showing: null},
+ *      ...
+ *   ]
+ */
+
+async function getCategory(catId) {
+	const response = await axios.get(`https://jservice.io/api/category?id=${catId}`);
+
+	let category = {
+		title: response.data.title,
+		clues: []
+	};
+
+	category.clues = response.data.clues.map((clue) => {
+		return {
+			question: clue.question,
+			amswer: clue.answer,
+			showing: null
+		};
+	});
+
+	return category;
+}
+
+/** Fill the HTML table#jeopardy with the categories & cells for questions.
+ *
+ * - The <thead> should be filled w/a <tr>, and a <td> for each category
+ * - The <tbody> should be filled w/NUM_QUESTIONS_PER_CAT <tr>s,
+ *   each with a question for each category in a <td>
+ *   (initally, just show a "?" where the question/answer would go.)
+ */
+
+async function fillTable() {}
+
+/** Handle clicking on a clue: show the question or answer.
+ *
+ * Uses .showing property on clue to determine what to show:
+ * - if currently null, show question & set .showing to "question"
+ * - if currently "question", show answer & set .showing to "answer"
+ * - if currently "answer", ignore click
+ * */
+
+function handleClick(evt) {}
+
+/** Wipe the current Jeopardy board, show the loading spinner,
+ * and update the button used to fetch data.
+ */
+
+function showLoadingView() {}
+
+/** Remove the loading spinner and update the button used to fetch data. */
+
+function hideLoadingView() {}
+
+/** Start game:
+ *
+ * - get random category Ids
+ * - get data for each category
+ * - create HTML table
+ * */
+
+async function setupAndStart() {}
+
+/** On click of start / restart button, set up game. */
+
+// TODO
+
+/** On page load, add event handler for clicking clues */
+
+// let categories = {
+//     title: response.data.title,
+//     clues: []
+// };
+
+// categories.clues = response.data.clues.map((clue) => {
+//     return {
+//         question: clue.question,
+//         answer: clue.answer
+//     };
+// });
