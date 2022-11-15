8c5c85f2e2ea0af7ed752071f2c7e7eec90096b3
Lauren Millan <laurenmillan@mydsn.net>
2022-11-15 11:07:34 -0500

    completed exercises, still need to complete convert.py file and words.py file

---
+++ JS-Assignments/python-syntax/words.py
@@ -0,0 +1,17 @@
+def print_upper_words():
+    """Print all words in uppercase letters.
+
+    For example:
+
+        print_upper_words(["hello", "hey", "goodbye", "yo", "yes"],
+                   must_start_with={"h", "y"})
+
+    should print:
+
+        "HELLO", "HEY", "YO", and "YES"
+    """
+
+    string = 'to uppercase'
+    print(string.upper())
+
+print_upper_words()
