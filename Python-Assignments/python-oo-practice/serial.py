2561f04e130d3816fe92d72588a45965a4c480f7
Lauren Millan <laurenmillan@mydsn.net>
2022-11-17 14:41:52 -0500

    completed SerialGenerator class

--- Python-Assignments/python-oo-practice/serial.py
+++ Python-Assignments/python-oo-practice/serial.py
@@ -20,3 +20,23 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start = 100):
+        """ Make serial number generator, with start initialized to 0 """
+
+        self.start = self.next = start
+
+    def __repr__(self):
+        """ Show representation """
+
+        return f"<SerialGenerator start = {self.start} next = {self.next}>"
+
+    def generate(self):
+        """ Return next serial number """
+
+        self.next += 1
+        return self.next - 1
+
+    def reset(self):
+        """ Reset serial number to initial start"""
+
+        self.next = self.start
