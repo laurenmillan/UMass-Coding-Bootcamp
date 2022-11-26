026bd2ad6da15ccd3d762256cd9c55aac8cd81aa
Lauren Millan <laurenmillan@mydsn.net>
2022-11-26 11:28:36 -0500

    updated code for home page

--- Flask-Framework/flask-survey/app.py
+++ Flask-Framework/flask-survey/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, render_template
-from surveys import surveys
+from flask import Flask, request, render_template, flash
+from surveys import satisfaction_survey as survey
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -8,13 +8,10 @@ app.config['SECRET_KEY'] = "secret"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-RESPONSES = []
+RESPONSES = 'responses'
 
 @app.route('/')
-def display_survey_page():
-    """Shows home page that displays title, instructions, and start button for survey."""
+def show_survey_start():
+    """Shows home page -- displays title, instructions, and start button for survey."""
 
-    return render_template('survey_page.html')
-
-
-
+    return render_template('survey_start.html', survey=survey)
