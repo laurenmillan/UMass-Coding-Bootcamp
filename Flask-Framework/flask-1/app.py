902ae2c0be9cd1e6e2cc1da824c12dfe6a3ffb93
Lauren Millan <laurenmillan@mydsn.net>
2022-11-29 16:26:44 -0500

    created test file

--- Flask-Framework/flask-1/app.py
+++ Flask-Framework/flask-1/app.py
@@ -1,13 +1,17 @@
-from flask import Flask, request, render_template, redirect, jsonify, session
+from flask import Flask, request, render_template, redirect, session, flash
+from currency_converter import CurrencyConverter
+from decimal import *
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+app.debug = True
 app.config['SECRET_KEY'] = 'secret'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def form_page():
     """Show form home page."""
 
