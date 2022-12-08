f7ef9a8fe281502809ec3b3d125b1084ebf5fedd
Lauren Millan <laurenmillan@mydsn.net>
2022-12-08 15:09:37 -0500

    wrote code to initialize alchemy, wrote a user model

--- sqlAlchemy/flask-blogly/app.py
+++ sqlAlchemy/flask-blogly/app.py
@@ -1,12 +1,26 @@
 """Blogly application."""
 
-from flask import Flask
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+
+# Here we configured the database url, then connect app with SQLAlchemy instance:
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_db'
+# Here we set this to false, so we don't see the deprecated warning message
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Here we set this to true, which prints all SQL statements to terminal -- helpful for debugging
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'secret123'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
-db.create_all()
+# db.create_all()
+
+@app.route('/')
+def home_page():
+    """Show home page."""
+
+    return render_template('home.html')
--- sqlAlchemy/flask-blogly/models.py
+++ sqlAlchemy/flask-blogly/models.py
@@ -1,1 +1,43 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+# initialize SQLAlchemy
+db = SQLAlchemy()
+
+# we put this in a function so we can call this logic to connect to a db from app.py -- best practice!
+def connect_db(app):
+    db.app = app 
+    db.init_app(app)
+
+# SQL version of table:
+
+# CREATE TABLE user (
+#     id SERIAL PRIMARY KEY,
+#     first_name VARCHAR(15) TEXT NOT NULL UNIQUE,
+#     last_name VARCHAR(15) TEXT NOT NULL UNIQUE,
+#     image_url TEXT
+# );
+
+# ORM -- Object-relational Mapping
+# MODELS GO BELOW, WHERE WE DEFINE OUR SCHEMA, USING SQLAlchemy
+# The point of a model is to create a class, which is a Python class
+
+class User(db.Model):
+    """User."""
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, 
+                            primary_key=True,
+                            autoincrement=True)
+
+    first_name = db.Column(db.String(15), 
+                            nullable=False, #first and last name cannot be Null
+                            unique=True)
+
+    last_name = db.Column(db.String(15), 
+                            nullable=False, 
+                            unique=True)
+
+    image_url = db.Column(db.String(50), nullable=True)
