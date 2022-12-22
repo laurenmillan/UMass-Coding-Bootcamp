347171567eb5ff59a5b8238afef731ae25497494
Lauren Millan <laurenmillan@mydsn.net>
2022-12-22 11:48:18 -0700

    added validators to forms file

--- Intermediate-Flask/Adoption-Agency/forms.py
+++ Intermediate-Flask/Adoption-Agency/forms.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, TextAreaField, BooleanField
+from wtforms.validators import InputRequired, Optional
 
 class AddPetForm(FlaskForm):
     """Form for adding pets."""
 
-    name = StringField('Pet Name')
-    species = StringField('Species')
-    photo_url = StringField('Photo URL')
-    age = IntegerField('Age')
-    notes = TextAreaField('Notes')
-    available = BooleanField('Available')
+    name = StringField('Pet Name', validators=[InputRequired(message="Pet name cannot be blank.")])
+    species = StringField('Species', validators=[InputRequired(message="Species cannot be blank.")])
+    photo_url = StringField('Photo URL', validators=[Optional()])
+    age = IntegerField('Age', validators=[InputRequired(message="Age cannot be blank.")])
+    notes = TextAreaField('Notes', validators=[Optional()])
+    available = BooleanField('Available', validators=[InputRequired(message="Please add if this pet is available for adoption.")])
