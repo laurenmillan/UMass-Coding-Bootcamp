0abe6481ee027463346160e0a5e9d217a4588184
Lauren Millan <laurenmillan@mydsn.net>
2023-01-14 14:08:49 -0500

    completed code for exercises one and two

--- Node Fundamentals/JS-Promises/Part II/app.js
+++ Node Fundamentals/JS-Promises/Part II/app.js
@@ -1,11 +1,11 @@
-//** Part I: make a request to the Deck of Cards API to request a single card from a newly shuffled deck: */
+//** Exercise I: make a request to the Deck of Cards API to request a single card from a newly shuffled deck: */
 let url = 'https://deckofcardsapi.com/';
 // $.getJSON(`${url}/api/deck/new/draw/?count=1`).then((data) => {
 // 	let { suit, value } = data.cards[0];
 // 	console.log(`${value} of ${suit}`);
 // });
 
-//** Part II: Make a request to the deck of cards API to request a single card from a newly shuffled deck. Once you have the card, make a request
+//** Exercise II: Make a request to the deck of cards API to request a single card from a newly shuffled deck. Once you have the card, make a request
 //  to the same API to get one more card from the same deck: */
 // let url = 'https://deckofcardsapi.com/';
 axios
@@ -23,7 +23,7 @@ axios
 		console.log(`${p3.data.cards[0].value} of ${p3.data.cards[0].suit}`);
 	});
 
-//** Part III: Build an HTML page that lets you draw cards from a deck.
+//** Exercise III: Build an HTML page that lets you draw cards from a deck.
 //  When the page loads, go to the Deck of Cards API to create a new deck, and show a button
 //  on the page that will let you draw a card. Every time you click the button,
 //  display a new card, until there are no cards left in the deck. */
