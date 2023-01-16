3445284965d8ceda1ddda05093c5bdd85af678da
Lauren Millan <laurenmillan@mydsn.net>
2023-01-16 12:16:20 -0500

    completed part one exercise

---
+++ Node Fundamentals/Async_Await/Part II/app.js
@@ -0,0 +1,9 @@
+//** Exercise I: make a request to the Deck of Cards API to request a single card from a newly shuffled deck: */
+
+//** Exercise II: Make a request to the deck of cards API to request a single card from a newly shuffled deck. Once you have the card, make a request
+//  to the same API to get one more card from the same deck: */
+
+//** Exercise III: Build an HTML page that lets you draw cards from a deck.
+//  When the page loads, go to the Deck of Cards API to create a new deck, and show a button
+//  on the page that will let you draw a card. Every time you click the button,
+//  display a new card, until there are no cards left in the deck. */
