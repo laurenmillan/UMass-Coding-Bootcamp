8c01d49491e4e982d6e0312ea8d371dcd2a6d3e9
Lauren Millan <laurenmillan@mydsn.net>
2023-01-20 11:29:12 -0500

    add completed code for markov machine exercise

--- Node Fundamentals/node-markov/makeText.js
+++ Node Fundamentals/node-markov/makeText.js
@@ -1,2 +1,49 @@
 /** Command-line tool to generate Markov text. */
 
+const markov = require('./markov');
+const fs = require('fs');
+const axios = require('axios');
+const process = require('process');
+
+function handleOutput(text) {
+	let mm = new markov.MarkovMachine(text);
+	console.log(mm.input());
+}
+
+function input(path, output) {
+	fs.readFile(path, 'utf8', (err, data) => {
+		if (err) {
+			console.log(`Error fetching ${path}: ${err}`);
+			process.exit(1);
+		} else {
+			handleOutput(data, output);
+		}
+	});
+}
+input(process.argv[2]);
+
+async function urlText(url, output) {
+	try {
+		let resp = axios.get(url);
+		handleOutput(resp.data, output);
+	} catch (err) {
+		console.log(`Error with ${path}: ${err}`);
+		process.exit(1);
+	}
+}
+
+let path;
+let output;
+
+if (process.argv[2] === '--out') {
+	output = process.argv[3];
+	path = process.argv[4];
+} else {
+	path = process.argv[2];
+}
+
+if (path.slice(0, 4) === 'http') {
+	urlText(path, output);
+} else {
+	cat(path, output);
+}
--- Node Fundamentals/node-markov/markov.js
+++ Node Fundamentals/node-markov/markov.js
@@ -1,29 +1,52 @@
 /** Textual markov chain generator */
 
+module.exports = class MarkovMachine {
+	/** build markov machine; read in text.*/
 
-class MarkovMachine {
+	constructor(text) {
+		let words = text.split(/[ \r\n]+/);
+		this.words = words.filter((c) => c !== '');
+		this.makeChains();
+	}
 
-  /** build markov machine; read in text.*/
-
-  constructor(text) {
-    let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
-    this.makeChains();
-  }
-
-  /** set markov chains:
+	/** set markov chains:
    *
    *  for text of "the cat in the hat", chains will be
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
-  makeChains() {
-    // TODO
-  }
+	makeChains() {
+		// TODO:DONE
+		let mm = new Map();
 
+		for (let i = 0; i < this.words.length; i += 1) {
+			let word = this.words[i];
+			let nextWord = this.words[i + 1] || null;
 
-  /** return random text from chains */
+			if (mm.has(word)) mm.get(word).push(nextWord);
+			else mm.set(word, [ nextWord ]);
+		}
+		this.mm = mm;
+	}
 
-  makeText(numWords = 100) {
-    // TODO
-  }
-}
+	/** return random word from arr */
+
+	static word(arr) {
+		return arr[Math.floor(Math.random() * arr.length)];
+	}
+
+	/** return random text from chains */
+
+	makeText(numWords = 100) {
+		// pick a random key to begin
+		let keys = Array.from(this.chains.keys());
+		let key = MarkovMachine.choice(keys);
+		let out = [];
+
+		// produce markov chain until reaching termination word
+		while (out.length < numWords && key !== null) {
+			out.push(key);
+			key = MarkovMachine.choice(this.chains.get(key));
+		}
+		return out.join(' ');
+	}
+};
