5a03a165fedd044b4dba2949a24eb65daf5351c1
Lauren Millan <laurenmillan@mydsn.net>
2023-02-01 12:38:30 -0500

    add starter code

---
+++ Intermediate-Express/express-messagely/routes/auth.js
@@ -0,0 +1,19 @@
+// const express = require('express');
+// const router = express.Router();
+// const ExpressError = require('../expressError');
+// const db = require('../db');
+
+// module.exports = router;
+
+/** POST /login - login: {username, password} => {token}
+ *
+ * Make sure to update their last-login!
+ *
+ **/
+
+/** POST /register - register user: registers, logs in, and returns token.
+ *
+ * {username, password, first_name, last_name, phone} => {token}.
+ *
+ *  Make sure to update their last-login!
+ */
---
+++ Intermediate-Express/express-messagely/routes/users.js
@@ -0,0 +1,38 @@
+// const express = require('express');
+// const router = express.Router();
+// const ExpressError = require('../expressError');
+// const db = require('../db');
+
+// module.exports = router;
+
+/** GET / - get list of users.
+ *
+ * => {users: [{username, first_name, last_name, phone}, ...]}
+ *
+ **/
+
+/** GET /:username - get detail of users.
+ *
+ * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
+ *
+ **/
+
+/** GET /:username/to - get messages to user
+ *
+ * => {messages: [{id,
+ *                 body,
+ *                 sent_at,
+ *                 read_at,
+ *                 from_user: {username, first_name, last_name, phone}}, ...]}
+ *
+ **/
+
+/** GET /:username/from - get messages from user
+ *
+ * => {messages: [{id,
+ *                 body,
+ *                 sent_at,
+ *                 read_at,
+ *                 to_user: {username, first_name, last_name, phone}}, ...]}
+ *
+ **/
