296dc883abaf26a8facbd0642056e1a55edf38c4
Lauren Millan <laurenmillan@mydsn.net>
2023-02-09 15:46:59 -0500

    add jobs route

--- express-jobly/routes/jobs.js
+++ express-jobly/routes/jobs.js
@@ -6,11 +6,124 @@ const jsonschema = require('jsonschema');
 const express = require('express');
 
 const { BadRequestError } = require('../expressError');
-const { ensureLoggedIn } = require('../middleware/auth');
-const Company = require('../models/company');
+const { ensureLoggedIn, ensureAdmin } = require('../middleware/auth');
+const Job = require('../models/job');
 
-const companyNewSchema = require('../schemas/companyNew.json');
-const companyUpdateSchema = require('../schemas/companyUpdate.json');
-const db = require('../db');
+const jobNewSchema = require('../schemas/jobNew.json');
+const jobUpdateSchema = require('../schemas/jobUpdate.json');
+const jobSearchSchema = require('../schemas/jobSearch.json');
 
 const router = new express.Router();
+
+/** POST / { job } =>  { job }
+ *
+ * job should be { title, salary, equity, company_handle }
+ *
+ * Returns { id, title, salary, equity, company_handle }
+ *
+ * Authorization required: admin
+ */
+
+router.post('/', ensureAdmin, async function(req, res, next) {
+	try {
+		const validator = jsonschema.validate(req.body, jobNewSchema);
+		if (!validator.valid) {
+			const errs = validator.errors.map((e) => e.stack);
+			throw new BadRequestError(errs);
+		}
+
+		const job = await Job.create(req.body);
+		return res.status(201).json({ job });
+	} catch (err) {
+		return next(err);
+	}
+});
+
+/** GET / all jobs  =>
+ *   { jobs: [ { id, title, salary, equity, company_handle }, ...] }
+ * Can filter on provided search filters:
+ * - minSalary
+ * - hasEquity
+ * - title
+ *
+ * Authorization required: none
+ */
+
+router.get('/', async function(req, res, next) {
+	let query = req.query;
+	// arrive as strings from querystring, but we want as ints
+	if (query.minSalary !== undefined) query.minSalary = +query.minSalary;
+	query.hasEquity = query.hasEquity === 'true';
+
+	try {
+		const validator = jsonschema.validate(query, jobSearchSchema);
+		if (!validator.valid) {
+			const errs = validator.errors.map((e) => e.stack);
+			throw new BadRequestError(errs);
+		} else {
+			const jobs = await Job.findAll(query);
+			return res.json({ jobs });
+		}
+	} catch (err) {
+		return next(err);
+	}
+});
+
+/** GET /[id]  =>  { job }
+ *
+ *  Job is { id, title, salary, equity, company }
+ *   where company is { handle, name, description, numEmployees, logoUrl }
+ *
+ * Authorization required: none
+ */
+
+router.get('/:id', async function(req, res, next) {
+	try {
+		const job = await Job.get(req.params.id);
+		return res.json({ job });
+	} catch (err) {
+		return next(err);
+	}
+});
+
+/** PATCH /[id] { fld1, fld2, ... } => { job }
+ *
+ * Patches job data.
+ *
+ * fields can be: { title, salary, equity }
+ *
+ * Returns { id, title, salary, equity, companyHandle }
+ *
+ * Authorization required: admin
+ */
+
+router.patch('/:id', ensureAdmin, async function(req, res, next) {
+	try {
+		const validator = jsonschema.validate(req.body, jobUpdateSchema);
+		if (!validator.valid) {
+			const errs = validator.errors.map((e) => e.stack);
+			throw new BadRequestError(errs);
+		}
+
+		const job = await Job.update(req.params.id, req.body);
+		return res.json({ job });
+	} catch (err) {
+		return next(err);
+	}
+});
+
+/** DELETE /[id]  =>  { deleted: id }
+ *
+ * Authorization: admin
+ */
+
+router.delete('/:id', ensureAdmin, async function(req, res, next) {
+	try {
+		await Job.remove(req.params.id);
+		return res.json({ deleted: req.params.id });
+	} catch (err) {
+		return next(err);
+	}
+});
+
+module.exports = router;
