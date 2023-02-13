cadc27f5694ef19283f95a7815ae69ae5c7bf438
Lauren Millan <laurenmillan@mydsn.net>
2023-02-13 11:21:45 -0500

    add starter code

---
+++ ReactJS/react-eight-ball/App.js
@@ -0,0 +1,8 @@
+const App = () => {
+	<div>
+		<EightBall name="hi" />
+	</div>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
---
+++ ReactJS/react-eight-ball/EightBall.js
@@ -0,0 +1,9 @@
+const EightBall = (props) => {
+	console.log(props);
+
+	return (
+		<div>
+			<h1>Name: {props.name} </h1>
+		</div>
+	);
+};
