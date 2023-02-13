ffc94a45ebc14bf005f7606e2aa4f173d4e40e1d
Lauren Millan <laurenmillan@mydsn.net>
2023-02-13 10:46:14 -0500

    add completed code for part III of exercise

--- ReactJS/react-jsx-intro/part-3/Person.js
+++ ReactJS/react-jsx-intro/part-3/Person.js
@@ -4,14 +4,15 @@ const Person = (props) => {
 	let hobbies = props.hobbies.map((hobbies) => <li>{hobbies}</li>);
 
 	return (
-		<div>
+		<div class="container-fluid">
 			<p>Learn some information about this person: </p>
 			<li>Name: {props.name.slice(0, 6)}</li>
 			<li>Age: {props.age}</li>
-			<h3 class="alert alert-primary" role="alert">
+			<h3 class="alert alert-info" role="alert">
 				{props.age >= 18 ? 'Please go vote!' : 'You must be 18'}
 			</h3>
-			<ul>Hobbies: {hobbies}</ul>
+			<ul class="container-fluid">Hobbies: {hobbies}</ul>
+			<hr />
 		</div>
 	);
 };
