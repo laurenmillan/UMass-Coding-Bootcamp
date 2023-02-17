30b616e6fe97e8adbd94f9b77c5f94c39f05cce8
Lauren Millan <laurenmillan@mydsn.net>
2023-02-17 09:58:34 -0500

    add complete Board component

--- ReactJS/react-lights-out/src/Board.js
+++ ReactJS/react-lights-out/src/Board.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import Cell from "./Cell";
-import "./Board.css";
+import React, { useState } from 'react';
+import Cell from './Cell';
+import './Board.css';
 
 /** Game board of Lights out.
  *
@@ -27,47 +27,81 @@ import "./Board.css";
  *
  **/
 
-function Board({ nrows, ncols, chanceLightStartsOn }) {
-  const [board, setBoard] = useState(createBoard());
+function Board({ nrows = 4, ncols = 4, chanceLightStartsOn = 0.3 }) {
+	const [ board, setBoard ] = useState(createBoard());
 
-  /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
-  function createBoard() {
-    let initialBoard = [];
-    // TODO: create array-of-arrays of true/false values
-    return initialBoard;
-  }
+	/** create a board nrows high/ncols wide, each cell randomly lit or unlit 
+   * createBoard randomly creates an array of arrays of true/false (Boolean) values to represent initial state of the game board.
+  */
+	function createBoard() {
+		let initialBoard = [];
 
-  function hasWon() {
-    // TODO: check the board in state to determine whether the player has won.
-  }
+		// create array-of-arrays of true/false values
+		for (let y = 0; y < nrows; y++) {
+			let row = [];
+			for (let x = 0; x < ncols; x++) {
+				row.push(Math.random() < chanceLightStartsOn);
+			}
+			initialBoard.push(row);
+		}
+		return initialBoard;
+	}
 
-  function flipCellsAround(coord) {
-    setBoard(oldBoard => {
-      const [y, x] = coord.split("-").map(Number);
+	function hasWon() {
+		// check the board in state to determine whether the player has won.
+		return board.every((row) => row.every((cell) => !cell));
+	}
 
-      const flipCell = (y, x, boardCopy) => {
-        // if this coord is actually on board, flip it
+	function flipCellsAround(coord) {
+		setBoard((oldBoard) => {
+			const [ y, x ] = coord.split('-').map(Number);
 
-        if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
-          boardCopy[y][x] = !boardCopy[y][x];
-        }
-      };
+			const flipCell = (y, x, boardCopy) => {
+				// if this coord is actually on board, flip it
 
-      // TODO: Make a (deep) copy of the oldBoard
+				if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
+					boardCopy[y][x] = !boardCopy[y][x];
+				}
+			};
 
-      // TODO: in the copy, flip this cell and the cells around it
+			// Make a (deep) copy of the oldBoard
+			const newBoard = oldBoard.map((row) => [ ...row ]);
 
-      // TODO: return the copy
-    });
-  }
+			// in the copy, flip this cell and the cells around it
+			flipCell(y, x, newBoard);
+			flipCell(y - 1, x, newBoard);
+			flipCell(y + 1, x, newBoard);
+			flipCell(y, x - 1, newBoard);
+			flipCell(y, x + 1, newBoard);
 
-  // if the game is won, just show a winning msg & render nothing else
+			// return the copy
+			return newBoard;
+		});
+	}
 
-  // TODO
+	// if the game is won, just show a winning msg & render nothing else
+	if (hasWon()) {
+		return <div>You won!</div>;
+	}
 
-  // make table board
-
-  // TODO
+	// make table board
+	return (
+		<table className="Board">
+			<tbody>
+				{board.map((row, y) => (
+					<tr key={y}>
+						{row.map((cell, x) => (
+							<Cell
+								key={`${y}-${x}`}
+								isLit={cell}
+								flipCellsAroundMe={() => flipCellsAround(`${y}-${x}`)}
+							/>
+						))}
+					</tr>
+				))}
+			</tbody>
+		</table>
+	);
 }
 
 export default Board;
--- ReactJS/react-lights-out/src/Board.test.js
+++ ReactJS/react-lights-out/src/Board.test.js
@@ -3,12 +3,13 @@ import { render } from '@testing-library/react';
 import Board from './Board';
 
 // SMOKE TEST
-test('it renders without crashing', () => {
-	render(<Board />);
+describe('Board', () => {
+	test('it renders without crashing', () => {
+		render(<Board />);
+	});
 });
 
 // SNAPSHOT TEST
-// Jest creates a __snapshots__ folder
 test('it matches snapshot', () => {
 	const { asFragment } = render(<Board />);
 	expect(asFragment()).toMatchSnapshot();
