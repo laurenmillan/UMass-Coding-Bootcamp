ab74e393d45baf460b6e983f9e654155a8bd5647
Lauren Millan <laurenmillan@mydsn.net>
2023-02-18 11:01:32 -0500

    add CRA skeleton with initial Components code

---
+++ ReactJS/react-to-do-app/src/NewTodoForm.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+/** Todo App that allows user to see, add, edit, and remove todos.
+ *
+ * State: 
+ * Props: 
+ *
+ * - this component should render a form with one text input for the task to be created. 
+ * - When this form is submitted, a new Todo component should be created. 
+ * - Todo- this component should display a div with the task of the todo.
+ * - For each Todo component, there should also be a button with the text “X” that when clicked, removes the todo.
+ *
+ **/
+
+export default NewTodoForm;
---
+++ ReactJS/react-to-do-app/src/TodoList.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import NewTodoForm from './NewTodoForm';
+
+/** Todo App that allows user to see, add, edit, and remove todos.
+ *
+ * State: 
+ * Props: 
+ *
+ * - this component should render the NewTodoForm component and should render the list of Todo components. 
+ * - Place your state that contains all of the todos in this component.
+ *
+ **/
+
+export default TodoList;
