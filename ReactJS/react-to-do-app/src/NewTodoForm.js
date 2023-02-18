abad094b04b4c27c73f466039bb8d2dbf0578f9a
Lauren Millan <laurenmillan@mydsn.net>
2023-02-18 12:00:16 -0500

    add completed Components

--- ReactJS/react-to-do-app/src/NewTodoForm.js
+++ ReactJS/react-to-do-app/src/NewTodoForm.js
@@ -2,14 +2,42 @@ import React, { useState } from 'react';
 
 /** Todo App that allows user to see, add, edit, and remove todos.
  *
- * State: 
- * Props: 
+ * State: formData, setFormData
+ * Props: addItem
  *
- * - this component should render a form with one text input for the task to be created. 
- * - When this form is submitted, a new Todo component should be created. 
+ * - this component should render a form with one text input for the task to be created.
+ * - When this form is submitted, a new Todo component should be created.
  * - Todo- this component should display a div with the task of the todo.
  * - For each Todo component, there should also be a button with the text “X” that when clicked, removes the todo.
  *
  **/
+
+const NewTodoForm = ({ addItem }) => {
+	const INITIAL_STATE = {
+		name: ''
+	};
+	const [ formData, setFormData ] = useState(INITIAL_STATE);
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setFormData((formData) => ({
+			...formData,
+			[name]: value
+		}));
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		addItem(formData.name);
+		// resets form back to initial state
+		setFormData(INITIAL_STATE);
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<label htmlFor="name">Todo</label>
+			<input id="name" type="text" name="name" value={formData.name} onChange={handleChange} />
+			<button>Add Todo</button>
+		</form>
+	);
+};
 
 export default NewTodoForm;
--- ReactJS/react-to-do-app/src/TodoList.js
+++ ReactJS/react-to-do-app/src/TodoList.js
@@ -1,14 +1,33 @@
 import React, { useState } from 'react';
+import Item from './Item';
 import NewTodoForm from './NewTodoForm';
 
 /** Todo App that allows user to see, add, edit, and remove todos.
  *
- * State: 
- * Props: 
+ * State: items, setItems
+ * Props: addItem
  *
  * - this component should render the NewTodoForm component and should render the list of Todo components. 
  * - Place your state that contains all of the todos in this component.
  *
  **/
+
+const TodoList = () => {
+	const INITIAL_STATE = [ { id: 1, name: 'Feed cat' }, { id: 2, name: 'Do laundry' } ];
+	const [ items, setItems ] = useState(INITIAL_STATE);
+
+	// this handles form data that is added to State
+	const addItem = (name) => {
+		// this creates a new array based off original items, and adds new item to the array
+		setItems((items) => [ ...items, { name } ]);
+	};
+	return (
+		<div>
+			<h3>Todo List</h3>
+			<NewTodoForm addItem={addItem} />
+			<div>{items.map((item) => <Item key={item.id} id={item.id} name={item.name} />)}</div>
+		</div>
+	);
+};
 
 export default TodoList;
