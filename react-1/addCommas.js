88f883db45578ac3bac734d3dd85de7f268f6b49
Lauren Millan <laurenmillan@mydsn.net>
2023-02-22 13:39:30 -0500

    add completed function and successful testing

--- react-1/addCommas.js
+++ react-1/addCommas.js
@@ -1,5 +1,33 @@
-import React from 'react';
+function addCommas(num) {
+	// Convert num to string, then store result in numStr
+	let numStr = num.toString();
 
-function addCommas() {}
+	// Check if num is negative by checking the first character of numStr, if num is negative, store it in isNegative, set to true
+	let isNegative = numStr.charAt(0) === '-';
+
+	// Split the string into int and decimal parts and store int in intPart and the decimal in decimalPart variables
+	let parts = numStr.split('.');
+	let intPart = isNegative ? parts[0].slice(1) : parts[0];
+	let decimalPart = parts[1] || '';
+
+	// Insert commas into the int part
+	// On the third iteration, when i is 5 and j is 3, a comma will be insterted before the current character, resulting in intWithCommas
+	let intWithCommas = '';
+	for (let i = intPart.length - 1, j = 1; i >= 0; i--, j++) {
+		if (j % 3 === 0 && i !== 0) {
+			intWithCommas = ',' + intPart.charAt(i) + intWithCommas;
+		} else {
+			intWithCommas = intPart.charAt(i) + intWithCommas;
+		}
+	}
+
+	// Concatenate the int and decimal parts with a decimal point
+	let result = isNegative ? '-' + intWithCommas : intWithCommas;
+	if (decimalPart !== '') {
+		result += '.' + decimalPart;
+	}
+
+	return result;
+}
 
 module.exports = addCommas;
--- react-1/addCommas.test.js
+++ react-1/addCommas.test.js
@@ -1,7 +1,39 @@
-const addCommas = require("./addCommas");
+const addCommas = require('./addCommas');
 
-describe("#addCommas", () => {
-  test("it is a function", () => {
-    expect(typeof addCommas).toBe("function");
-  });
+describe('#addCommas', () => {
+	test('it is a function', () => {
+		expect(typeof addCommas).toBe('function');
+	});
+});
+
+test('adds commas to a positive integer', () => {
+	expect(addCommas(1234)).toBe('1,234');
+});
+
+test('adds commas to a large positive integer', () => {
+	expect(addCommas(1000000)).toBe('1,000,000');
+});
+
+test('adds commas to a very large positive integer', () => {
+	expect(addCommas(9876543210)).toBe('9,876,543,210');
+});
+
+test('does not add commas to a single-digit integer', () => {
+	expect(addCommas(6)).toBe('6');
+});
+
+test('adds a negative sign to a negative integer', () => {
+	expect(addCommas(-10)).toBe('-10');
+});
+
+test('adds commas to a negative integer', () => {
+	expect(addCommas(-5678)).toBe('-5,678');
+});
+
+test('adds commas to a positive decimal number', () => {
+	expect(addCommas(12345.678)).toBe('12,345.678');
+});
+
+test('adds commas to a negative decimal number', () => {
+	expect(addCommas(-3141592.65)).toBe('-3,141,592.65');
 });
