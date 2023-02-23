cb25325b4eef0197dac7d6e3214e2181e60da852
Lauren Millan <laurenmillan@mydsn.net>
2023-02-23 17:00:59 -0500

    add complete Components with navlinks

--- React-Router/vending-machine/src/App.js
+++ React-Router/vending-machine/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
+import VendingMachine from './VendingMachine';
+import LifeSavers from './LifeSavers';
+import Skittles from './Skittles';
+import Starburst from './Starburst';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route exact path="/" element={<VendingMachine />} />
+					<Route exact path="/LifeSavers" element={<LifeSavers />} />
+					<Route exact path="/Skittles" element={<Skittles />} />
+					<Route exact path="/Starburst" element={<Starburst />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ React-Router/vending-machine/src/LifeSavers.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const LifeSavers = () => {
+	return (
+		<div className="LifeSavers">
+			<div>🍭</div>
+			<NavLink to="/">Go Back</NavLink>
+		</div>
+	);
+};
+
+export default LifeSavers;
