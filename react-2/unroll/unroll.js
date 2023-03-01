be170c5d241bdb579d38811bbe0c9b4bdb50976e
Lauren Millan <laurenmillan@mydsn.net>
2023-03-01 11:42:43 -0500

    add complete code for part II

--- react-2/unroll/unroll.js
+++ react-2/unroll/unroll.js
@@ -1,5 +1,43 @@
 function unroll(squareArray) {
+	const result = [];
 
+	let rowStart = 0,
+		// set to last index of array
+		rowEnd = squareArray.length - 1;
+	let colStart = 0,
+		colEnd = squareArray[0].length - 1;
+
+	while (rowStart <= rowEnd && colStart <= colEnd) {
+		// right
+		for (let i = colStart; i <= colEnd; i++) {
+			result.push(squareArray[rowStart][i]);
+		}
+		rowStart++;
+
+		// down
+		for (let i = rowStart; i <= rowEnd; i++) {
+			result.push(squareArray[i][colEnd]);
+		}
+		colEnd--;
+
+		// left
+		if (rowStart <= rowEnd) {
+			for (let i = colEnd; i >= colStart; i--) {
+				result.push(squareArray[rowEnd][i]);
+			}
+			rowEnd--;
+		}
+
+		// up
+		if (colStart <= colEnd) {
+			for (let i = rowEnd; i >= rowStart; i--) {
+				result.push(squareArray[i][colStart]);
+			}
+			colStart++;
+		}
+	}
+
+	return result;
 }
 
 module.exports = unroll;
--- react-2/unroll/unroll.test.js
+++ react-2/unroll/unroll.test.js
@@ -1,9 +1,30 @@
-const unroll = require("./unroll");
+const unroll = require('./unroll');
 
-describe("#unroll", function () {
+describe('#unroll', () => {
+	test('is a function', function() {
+		expect(typeof unroll).toEqual('function');
+	});
 
-  it("is a function", function () {
-    expect(typeof unroll).toEqual("function");
-  });
+	test('returns an empty array for an empty input', () => {
+		expect(unroll([])).toEqual([]);
+	});
 
+	test('returns an array with one element for a one-element input', () => {
+		expect(unroll([ [ 1 ] ])).toEqual([ 1 ]);
+	});
+
+	test('returns the correct result for a 2x2 input', () => {
+		const squareArray = [ [ 1, 2 ], [ 3, 4 ] ];
+		expect(unroll(squareArray)).toEqual([ 1, 2, 4, 3 ]);
+	});
+
+	test('returns the correct result for a 3x3 input', () => {
+		const squareArray = [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ];
+		expect(unroll(squareArray)).toEqual([ 1, 2, 3, 6, 9, 8, 7, 4, 5 ]);
+	});
+
+	test('returns the correct result for a 4x4 input', () => {
+		const squareArray = [ [ 1, 2, 3, 4 ], [ 5, 6, 7, 8 ], [ 9, 10, 11, 12 ], [ 13, 14, 15, 16 ] ];
+		expect(unroll(squareArray)).toEqual([ 1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10 ]);
+	});
 });
