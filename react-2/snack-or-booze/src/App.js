1ed26b6bc4752d1c6145be2b6539c18bb15a4604
Lauren Millan <laurenmillan@mydsn.net>
2023-03-01 14:56:27 -0500

    add changes

--- react-2/snack-or-booze/src/App.js
+++ react-2/snack-or-booze/src/App.js
@@ -1,53 +1,83 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter } from "react-router-dom";
-import "./App.css";
-import Home from "./Home";
-import SnackOrBoozeApi from "./Api";
-import NavBar from "./NavBar";
-import { Route, Switch } from "react-router-dom";
-import Menu from "./FoodMenu";
-import Snack from "./FoodItem";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import Home from './Home';
+import SnackOrBoozeApi from './Api';
+import NavBar from './NavBar';
+import { Route, Switch } from 'react-router-dom';
+import Menu from './Menu';
+import Snack from './Item';
+import AddNew from './AddNew';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [snacks, setSnacks] = useState([]);
+	const [ isLoading, setIsLoading ] = useState(true);
+	const [ snacks, setSnacks ] = useState([]);
+	const [ drinks, setDrinks ] = useState([]);
 
-  useEffect(() => {
-    async function getSnacks() {
-      let snacks = await SnackOrBoozeApi.getSnacks();
-      setSnacks(snacks);
-      setIsLoading(false);
-    }
-    getSnacks();
-  }, []);
+	useEffect(() => {
+		async function getSnacks() {
+			let snacks = await SnackOrBoozeApi.getSnacks('snacks');
+			let drinks = await SnackOrBoozeApi.getSnacks('drinks');
+			setSnacks(snacks);
+			setSnacks(drinks);
+			setIsLoading(false);
+		}
+		getSnacks();
+	}, []);
 
-  if (isLoading) {
-    return <p>Loading &hellip;</p>;
-  }
+	const updateState = (item) => {
+		const { type, description, name, recipe, serve } = item;
+		const id = name.toLowerCase().replace('', '-');
+		if (type === 'snack') {
+			setSnacks((snack) => [ ...snacks, { id, name, description, recipe, serve } ]);
+		} else {
+			setDrinks((drink) => [ ...drinks, { id, name, description, recipe, serve } ]);
+		}
+	};
 
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <NavBar />
-        <main>
-          <Switch>
-            <Route exact path="/">
-              <Home snacks={snacks} />
-            </Route>
-            <Route exact path="/snacks">
-              <Menu snacks={snacks} title="Snacks" />
-            </Route>
-            <Route path="/snacks/:id">
-              <Snack items={snacks} cantFind="/snacks" />
-            </Route>
-            <Route>
-              <p>Hmmm. I can't seem to find what you want.</p>
-            </Route>
-          </Switch>
-        </main>
-      </BrowserRouter>
-    </div>
-  );
+	if (isLoading) {
+		return <p>Loading &hellip;</p>;
+	}
+
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<NavBar />
+				<main>
+					<Switch>
+						<Route exact path="/">
+							<Home snacks={snacks} drinks={drinks} />
+						</Route>
+						<Route exact path="/home">
+							<Home snacks={snacks} drinks={drinks} />
+						</Route>
+
+						<Route exact path="/snacks">
+							<Menu items={snacks} path="snacks" title="Snacks" />
+						</Route>
+						<Route path="/snacks/:id">
+							<Snack items={snacks} cantFind="/snacks" />
+						</Route>
+
+						<Route exact path="/drinks">
+							<Menu items={drinks} path="drinks" title="Drinks" />
+						</Route>
+						<Route path="/drinks/:id">
+							<Drink items={drinks} cantFind="/drinks" />
+						</Route>
+
+						<Route path="/add">
+							<AddNew updateState={updateState} snacks={snacks} drinks={drinks} />
+						</Route>
+
+						<Route path="/*">
+							<NotFound />
+						</Route>
+					</Switch>
+				</main>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
--- react-2/snack-or-booze/src/Home.js
+++ react-2/snack-or-booze/src/Home.js
@@ -1,20 +1,21 @@
-import React from "react";
-import { Card, CardBody, CardTitle } from "reactstrap";
+import React from 'react';
+import { Card, CardBody, CardTitle } from 'reactstrap';
 
-function Home() {
-  return (
-    <section className="col-md-8">
-      <Card>
-        <CardBody className="text-center">
-          <CardTitle>
-            <h3 className="font-weight-bold">
-              Welcome to Silicon Valley's premier dive cafe!
-            </h3>
-          </CardTitle>
-        </CardBody>
-      </Card>
-    </section>
-  );
+function Home({ snacks, drinks }) {
+	return (
+		<section className="col-md-8">
+			<Card>
+				<CardBody className="text-center">
+					<CardTitle>
+						<h3 className="font-weight-bold">Welcome to Silicon Valley's premier dive cafe!</h3>
+						<p>
+							Serving {snacks.length} & {drinks.length} items!
+						</p>
+					</CardTitle>
+				</CardBody>
+			</Card>
+		</section>
+	);
 }
 
 export default Home;
