3eec2e2e69074bbda7f68b158b69e22273e94e0c
Lauren Millan <laurenmillan@mydsn.net>
2023-03-02 09:58:13 -0500

    add Drinks link to navbar, add Drinks Menu page

--- react-2/snack-or-booze/src/FoodMenu.js
+++ react-2/snack-or-booze/src/FoodMenu.js
@@ -3,19 +3,27 @@ import { Link } from 'react-router-dom';
 import './FoodMenu.css';
 import { Card, CardBody, CardTitle, CardText, ListGroup, ListGroupItem } from 'reactstrap';
 
-function FoodMenu({ snacks }) {
+function FoodMenu({ snacks, drinks }) {
+	let items;
+	let link;
+	if (snacks) {
+		items = snacks;
+		link = 'Snacks';
+	} else {
+		items = drinks;
+		link = 'Drinks';
+	}
+
 	return (
 		<section className="col-md-4">
 			<Card>
 				<CardBody>
-					<CardTitle className="font-weight-bold text-center">Food Menu</CardTitle>
-					<CardText>
-						Some quick example text to build on the card title and make up the bulk of the card's content.
-					</CardText>
+					<CardTitle className="font-weight-bold text-center">{link} Menu</CardTitle>
+					<CardText>Check out all of our {link}!</CardText>
 					<ListGroup>
-						{snacks.map((snack) => (
-							<Link to={`/snacks/${snack.id}`} key={snack.id}>
-								<ListGroupItem>{snack.name}</ListGroupItem>
+						{items.map((item) => (
+							<Link to={`/${link}/${item.id}`} key={item.id}>
+								<ListGroupItem>{item.name}</ListGroupItem>
 							</Link>
 						))}
 					</ListGroup>
