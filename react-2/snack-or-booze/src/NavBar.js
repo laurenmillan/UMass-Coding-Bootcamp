a80a63cf9b3c95313fc04cdda20687d63e72a55c
Lauren Millan <laurenmillan@mydsn.net>
2023-03-02 09:12:07 -0500

    add change, Mac's icloud messing up Git branch

--- react-2/snack-or-booze/src/NavBar.js
+++ react-2/snack-or-booze/src/NavBar.js
@@ -13,21 +13,8 @@ function NavBar() {
 
 				<Nav className="ml-auto" navbar>
 					<NavItem>
-<<<<<<< HEAD
 						<NavLink to="/snacks">Snacks</NavLink>
 					</NavItem>
-=======
-						<NavLink to="/home">Home</NavLink>
-					</NavItem>
-
-					<NavItem>
-						<NavLink to="/snacks">Snacks</NavLink>
-					</NavItem>
-
-					<NavItem>
-						<NavLink to="/drinks">Drinks</NavLink>
-					</NavItem>
->>>>>>> 1ed26b6bc4752d1c6145be2b6539c18bb15a4604
 				</Nav>
 			</Navbar>
 		</div>
