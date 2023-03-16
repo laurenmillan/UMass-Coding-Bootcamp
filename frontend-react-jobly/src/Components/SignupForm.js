13f50fa8c288734e515e4f58ad6a161d53b37405
Lauren Millan <laurenmillan@mydsn.net>
2023-03-16 13:54:04 -0400

    add updated styles

--- frontend-react-jobly/src/Components/SignupForm.js
+++ frontend-react-jobly/src/Components/SignupForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Card } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
 /** Renders a Signup Form. 
@@ -60,85 +60,87 @@ const SignupForm = ({ signup }) => {
 	return (
 		<div className="SignupForm">
 			<div className="container col-md-6 offset-md-3 col-lg-4 offset-lg-4">
-				<h2 className="mb-3" style={{ color: 'white', textShadow: '1px 1px black' }}>
-					Sign Up
-				</h2>
-				<Form noValidate validated={validated} onSubmit={handleSubmit}>
-					<Form.Group>
-						<Form.Label htmlFor="username">Username</Form.Label>
-						<Form.Control
-							type="text"
-							name="username"
-							value={formData.username}
-							onChange={handleChange}
-							autoComplete="username"
-							required
-							aria-describedby="username-description"
-							className="w-100"
-						/>
-					</Form.Group>
+				<Card>
+					<Card.Body>
+						<h2 className="mb-3">Sign Up</h2>
+						<Form noValidate validated={validated} onSubmit={handleSubmit}>
+							<Form.Group className="mb-3">
+								<Form.Label htmlFor="username">Username</Form.Label>
+								<Form.Control
+									type="text"
+									name="username"
+									value={formData.username}
+									onChange={handleChange}
+									autoComplete="username"
+									required
+									aria-describedby="username-description"
+									className="w-100"
+								/>
+							</Form.Group>
 
-					<Form.Group>
-						<Form.Label htmlFor="password">Password</Form.Label>
-						<Form.Control
-							type="password"
-							name="password"
-							value={formData.password}
-							onChange={handleChange}
-							autoComplete="current-password"
-							required
-							aria-describedby="password-description"
-							minLength={5}
-							className="w-100"
-						/>
-						<Form.Control.Feedback type="invalid">
-							Password must be at least 5 characters.
-						</Form.Control.Feedback>
-					</Form.Group>
+							<Form.Group className="mb-3">
+								<Form.Label htmlFor="password">Password</Form.Label>
+								<Form.Control
+									type="password"
+									name="password"
+									value={formData.password}
+									onChange={handleChange}
+									autoComplete="current-password"
+									required
+									aria-describedby="password-description"
+									minLength={5}
+									className="w-100"
+								/>
+								<Form.Control.Feedback type="invalid">
+									Password must be at least 5 characters.
+								</Form.Control.Feedback>
+							</Form.Group>
 
-					<Form.Group>
-						<Form.Label htmlFor="first-name">First Name</Form.Label>
-						<Form.Control
-							type="text"
-							name="firstName"
-							value={formData.firstName}
-							onChange={handleChange}
-							required
-							aria-describedby="first-name"
-							className="w-100"
-						/>
-					</Form.Group>
+							<Form.Group className="mb-3">
+								<Form.Label htmlFor="first-name">First Name</Form.Label>
+								<Form.Control
+									type="text"
+									name="firstName"
+									value={formData.firstName}
+									onChange={handleChange}
+									required
+									aria-describedby="first-name"
+									className="w-100"
+								/>
+							</Form.Group>
 
-					<Form.Group>
-						<Form.Label htmlFor="last-name">Last Name</Form.Label>
-						<Form.Control
-							type="text"
-							name="lastName"
-							value={formData.lastName}
-							onChange={handleChange}
-							required
-							aria-describedby="last-name"
-							className="w-100"
-						/>
-					</Form.Group>
+							<Form.Group className="mb-3">
+								<Form.Label htmlFor="last-name">Last Name</Form.Label>
+								<Form.Control
+									type="text"
+									name="lastName"
+									value={formData.lastName}
+									onChange={handleChange}
+									required
+									aria-describedby="last-name"
+									className="w-100"
+								/>
+							</Form.Group>
 
-					<Form.Group>
-						<Form.Label htmlFor="email">Email</Form.Label>
-						<Form.Control
-							type="email"
-							name="email"
-							value={formData.email}
-							onChange={handleChange}
-							required
-							aria-describedby="email"
-							className="w-100"
-						/>
-					</Form.Group>
+							<Form.Group className="mb-3">
+								<Form.Label htmlFor="email">Email</Form.Label>
+								<Form.Control
+									type="email"
+									name="email"
+									value={formData.email}
+									onChange={handleChange}
+									required
+									aria-describedby="email"
+									className="w-100"
+								/>
+							</Form.Group>
 
-					<Button variant="primary" type="submit">
-						Sign Up
-					</Button>
-				</Form>
+							<Button variant="primary" type="submit">
+								Sign Up
+							</Button>
+						</Form>
+					</Card.Body>
+				</Card>
 			</div>
 		</div>
 	);
