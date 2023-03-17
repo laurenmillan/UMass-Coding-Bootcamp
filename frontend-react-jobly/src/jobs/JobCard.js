d00b1d9739ccdb68db269755d515008bfe2a0da2
Lauren Millan <laurenmillan@mydsn.net>
2023-03-17 15:08:58 -0400

    add logic for the applied job remains in LS and user can't apply to the same job

--- frontend-react-jobly/src/jobs/JobCard.js
+++ frontend-react-jobly/src/jobs/JobCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './JobCard.css';
 import { FormatMoney } from 'format-money-js';
 
@@ -8,6 +8,7 @@ import { FormatMoney } from 'format-money-js';
  * -props: id, title, salary, equity, companyName
  * -Receives the apply function prop from the parent which is called on apply.
  * -Routed from JobCardList -> JobCard
+ * -If a user applies to a job, the `Applied` button will persist across page refreshes, store state in local storage.
  * 
  */
 
@@ -16,12 +17,25 @@ function JobCard({ id, title, salary, equity, companyName, applyToJob }) {
 
 	const [ hasApplied, setHasApplied ] = useState(false);
 
+	// Retrieve the applied state from local storage
+	useEffect(
+		() => {
+			const storedAppliedState = localStorage.getItem(`job-${id}-applied`);
+			if (storedAppliedState) {
+				setHasApplied(JSON.parse(storedAppliedState));
+			}
+		},
+		[ id ]
+	);
+
 	async function handleApply() {
 		console.debug('Successfully Applied');
 		if (hasApplied) return;
 
+		// store in local storage
 		await applyToJob(id);
 		setHasApplied(true);
+		localStorage.setItem(`job-${id}-applied`, JSON.stringify(true));
 	}
 
 	const fm = new FormatMoney({
