31492159fa0a82bb7e89567c9f729797f692c99f
Lauren Millan <laurenmillan@mydsn.net>
2023-03-28 10:42:57 -0400

    add completed code for stacks exercise

--- dsa-stacks-queues/stack.js
+++ dsa-stacks-queues/stack.js
@@ -1,46 +1,68 @@
 /** Node: node for a stack. */
 
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
 }
 
 /** Stack: chained-together nodes where you can
  *  remove from the top or add to the top. */
 
 class Stack {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
 
-  /** push(val): add new value to end of the stack. Returns undefined. */
+	/** push(val): add new value to end of the stack. Returns undefined. */
 
-  push(val) {
+	push(val) {
+		const newNode = new Node(val);
 
-  }
+		if (!this.first) {
+			this.first = newNode;
+			this.last = newNode;
+		} else {
+			newNode.next = this.first;
+			this.first = newNode;
+		}
+		this.size++;
+	}
 
-  /** pop(): remove the node from the top of the stack
+	/** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
-  pop() {
+	pop() {
+		if (!this.first) {
+			throw new Error('Stack is empty');
+		}
+		const removedNode = this.first;
+		if (this.first === this.last) {
+			this.last = null;
+		}
+		this.first = this.first.next;
+		this.size--;
 
-  }
+		return removedNode.val;
+	}
 
-  /** peek(): return the value of the first node in the stack. */
+	/** peek(): return the value of the first node in the stack. */
 
-  peek() {
+	peek() {
+		if (!this.first) {
+			throw new Error('Stack is empty');
+		}
+		return this.first.val;
+	}
 
-  }
+	/** isEmpty(): return true if the stack is empty, otherwise false */
 
-  /** isEmpty(): return true if the stack is empty, otherwise false */
-
-  isEmpty() {
-
-  }
+	isEmpty() {
+		return this.size === 0;
+	}
 }
 
 module.exports = Stack;
