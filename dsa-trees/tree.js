ab86cbe5ae71ae8df17fd82e029cf60b1a419039
Lauren Millan <laurenmillan@mydsn.net>
2023-03-30 09:27:03 -0400

    add complete code for tree exercise

--- dsa-trees/tree.js
+++ dsa-trees/tree.js
@@ -1,35 +1,60 @@
 /** TreeNode: node for a general tree. */
 
 class TreeNode {
-  constructor(val, children = []) {
-    this.val = val;
-    this.children = children;
-  }
+	constructor(val, children = []) {
+		this.val = val;
+		this.children = children;
+	}
 }
 
 class Tree {
-  constructor(root = null) {
-    this.root = root;
-  }
+	constructor(root = null) {
+		this.root = root;
+	}
 
-  /** sumValues(): add up all of the values in the tree. */
+	/** sumValues(): add up all of the values in the tree. */
 
-  sumValues() {
-    
-  }
+	sumValues(node = this.root) {
+		if (!node) return 0; // check if node is null
 
-  /** countEvens(): count all of the nodes in the tree with even values. */
+		let sum = node.val; // initialize sum to the value of the current node
+		for (const child of node.children) {
+			sum += this.sumValues(child); // calls sumValues function and adds the result to the sum variable
+		}
 
-  countEvens() {
+		return sum;
+	}
 
-  }
+	/** countEvens(): count all of the nodes in the tree with even values. */
 
-  /** numGreater(lowerBound): return a count of the number of nodes
+	countEvens(node = this.root) {
+		if (!node) return 0;
+
+		// initialize count with a value of 1 if the current node's value is even, or 0 otherwise
+		// this initializes the count of even-valued nodes for this node's subtree
+		let count = node.val % 2 === 0 ? 1 : 0;
+		for (const child of node.children) {
+			count += this.countEvens(child);
+		}
+
+		return count;
+	}
+
+	/** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
-  numGreater(lowerBound) {
+	numGreater(lowerBound, node = this.root) {
+		if (!node) return 0;
 
-  }
+		// initializes count with a value of 1 if the current node's value is >lowerBound, or 0 otherwise
+		// this initializes the count of nodes with values >lower bound for this node's subtree
+		let count = node.val > lowerBound ? 1 : 0;
+		for (const child of node.children) {
+			count += this.numGreater(lowerBound, child);
+		}
+
+		return count;
+	}
 }
 
 module.exports = { Tree, TreeNode };
